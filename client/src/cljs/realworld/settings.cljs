dfc45192e5292e92a076743f39437e204d15f9cd
David Viramontes <dviramontes@gmail.com>
2017-12-02 14:16:48 -0700

    feat: point to local dev environment when devving

--- client/src/cljs/realworld/settings.cljs
+++ client/src/cljs/realworld/settings.cljs
@@ -1,5 +1,10 @@
 (ns realworld.settings)
 
-(def api-endpoint "https://conduit.productionready.io/api")
+(def debug? ^boolean goog.DEBUG)
+
+(def api-endpoint (if debug?
+                    "http://localhost:4000"
+                    "https://conduit.productionready.io/api"))
+
 (def articles-per-page 10)
 (def jwt-local-storage-name "conduit-jwt-token")
