06734b71149046b4cec42fc5ad0f3b6f12eb65ab
David Viramontes <dviramontes@gmail.com>
2017-12-03 01:04:27 -0700

    fix: overwrite init method

--- lib/real_world/repo.ex
+++ lib/real_world/repo.ex
@@ -5,7 +5,18 @@ defmodule RealWorld.Repo do
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+
+#  def init(_, opts) do
+#    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+#  end
+
+  def init(_key, config) do
+    if config[:load_from_system_env] do
+      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+    else
+      {:ok, config}
+    end
   end
+
 end
